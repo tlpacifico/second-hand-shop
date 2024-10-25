27efd660269d76ab8f549bb71086750c44fde29b
tlpoliveira <thacio.pacifico@gmail.com>
2024-10-25 23:18:29 +0100

    created consignment endponts and added scalar ui support

--- src/shs.Api/Presentation/Endpoints/ConsignmentSupplierEndpoints.cs
+++ src/shs.Api/Presentation/Endpoints/ConsignmentSupplierEndpoints.cs
@@ -1,6 +1,52 @@
-﻿namespace shs.Api.Presentation.Endpoints;
+﻿using Microsoft.EntityFrameworkCore;
+using shs.Api.Domain.Entities;
+using shs.Api.Infrastructure.Database;
 
-public class ConsignmentSupplierEndpoints
+namespace shs.Api.Presentation.Endpoints;
+
+public static class ConsignmentSupplierEndpoints
 {
-    
+    public static void MapConsignmentsEndpoints(this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("/consignments");
+
+        group.MapGet("/suppliers", async (ShsDbContext db) =>
+            await db.ConsignmentSuppliers.ToListAsync());
+
+        group.MapGet("/suppliers/{id}", async (ShsDbContext db, long id) =>
+            await db.ConsignmentSuppliers.FindAsync(id) is ConsignmentSupplierEntity supplier
+                ? Results.Ok(supplier)
+                : Results.NotFound());
+
+        group.MapPost("/suppliers", async (ShsDbContext db, ConsignmentSupplierEntity supplier) =>
+        {
+            db.ConsignmentSuppliers.Add(supplier);
+            await db.SaveChangesAsync();
+            return Results.Created($"/consignmentSuppliers/{supplier.Id}", supplier);
+        });
+
+        group.MapPut("/suppliers/{id}", async (ShsDbContext db, long id, ConsignmentSupplierEntity updatedSupplier) =>
+        {
+            var supplier = await db.ConsignmentSuppliers.FindAsync(id);
+            if (supplier is null) return Results.NotFound();
+
+            supplier.Name = updatedSupplier.Name;
+            supplier.Email = updatedSupplier.Email;
+            supplier.PhoneNumber = updatedSupplier.PhoneNumber;
+            supplier.Address = updatedSupplier.Address;
+
+            await db.SaveChangesAsync();
+            return Results.NoContent();
+        });
+
+        group.MapDelete("/suppliers/{id}", async (ShsDbContext db, long id) =>
+        {
+            var supplier = await db.ConsignmentSuppliers.FindAsync(id);
+            if (supplier is null) return Results.NotFound();
+
+            db.ConsignmentSuppliers.Remove(supplier);
+            await db.SaveChangesAsync();
+            return Results.NoContent();
+        });
+    }
 }
--- src/shs.Api/Program.cs
+++ src/shs.Api/Program.cs
@@ -1,41 +1,43 @@
+using Microsoft.AspNetCore.Identity;
+using shs.Api;
+using shs.Api.Domain.Entities;
+using shs.Api.Infrastructure.Database;
+using shs.Api.Presentation.Endpoints;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi("v1");
+builder.Services.AddDatabaseInfrastructure(builder.Configuration);
+builder.Services.AddHttpContextAccessor();
 
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication()
+    .AddCookie(IdentityConstants.ApplicationScheme);
+
+builder.Services.AddIdentityCore<UserEntity>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = true;
+}).AddEntityFrameworkStores<ShsDbContext>()
+.AddApiEndpoints();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
+app.MapConsignmentsEndpoints();
+using var scope = app.Services.CreateScope();
+await scope.ApplyMigrations();
 
+app.MapIdentityApi<UserEntity>();
+app.MapScalarUi();
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
